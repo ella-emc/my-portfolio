ced210abd7075bb20e0da80e5e3635f91306f722
Ella Mae <ellalala.mc@gmail.com>
2024-06-11 20:50:30 +0800

    build: Set up portfolio project

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js}",
+    "./*.html",
+    "./*.jsx"
+  ],  
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
