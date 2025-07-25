4151bb946097c8a35565273a5c1d8ba141a07599
Ella <ellalala.mc@gmail.com>
2025-07-25 21:34:51 +0800

    style: :lipstick: Apply text styles

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -2,8 +2,8 @@ import { TypeAnimation } from "react-type-animation";
 
 const HeroSection = () => {
   return (
-    <section className='flex flex-col items-center justify-center'>
-      <p className='text-accent font-space-grotesk'>Hi, I'm Ella</p>
+    <section className='flex flex-col items-center justify-center py-[100px] px-16 lg:py-60 lg:px-80 gap-y-3 lg:gap-y-6'>
+      <p className='text-accent font-space-grotesk text-base lg:text-xl'>Hi, I'm Ella</p>
       <TypeAnimation 
         sequence={[
           'A web developer',
@@ -23,11 +23,20 @@ const HeroSection = () => {
           backgroundClip: "text",
           color: "transparent", 
         }}
-        className="font-space-grotesk font-bold"
+        className="font-space-grotesk font-bold text-center leading-tight text-[42px] lg:text-6xl"
         repeat={Infinity}
         cursor={true}
       />
-      <p className='font-space-grotesk text-text-low-contrast'>I design <span className='font-bold text-text'>colorful</span> interfaces and develop <span className='font-bold text-text'>dynamic</span> web and mobile applications.</p>
+      <p className='font-space-grotesk text-center text-text-low-contrast text-base lg:text-2xl lg:w-2/3'>
+        I design 
+        <span className='font-bold text-text'> colorful </span> 
+        interfaces and develop 
+        <span className='font-bold text-text'> dynamic </span> 
+        web and mobile applications.
+      </p>
+      <button className="mt-6 bg-primary font-sora text-text rounded-2xl py-3 px-16 text-base lg:text-lg">
+        Contact me
+      </button>
     </section>
   )
 }
