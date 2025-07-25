bf0ddb2edb13b09e6ed8a76181e7802b70b90673
Ella <ellalala.mc@gmail.com>
2025-07-25 21:11:11 +0800

    feat: :sparkles: Add TypeAnimation sequence

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,10 +1,32 @@
-import React from 'react';
+import { TypeAnimation } from "react-type-animation";
 
 const HeroSection = () => {
   return (
     <section className='flex flex-col items-center justify-center'>
       <p className='text-accent font-space-grotesk'>Hi, I'm Ella</p>
-      <h1 className='text-text font-space-grotesk'>A web developer</h1>
+      <TypeAnimation 
+        sequence={[
+          'A web developer',
+          1500,
+          'A mobile developer',
+          1500,
+          'A full-stack developer',
+          1500,
+          'An AI and data science enthusiast',
+          1500
+        ]}
+        wrapper="span"
+        speed={{ type: "keyStrokeDelayInMs", value: 90 }}
+        style={{
+          backgroundImage: "linear-gradient(to right, #A35CF0 0%, #F0FF6F 100%)",
+          WebkitBackgroundClip: "text", 
+          backgroundClip: "text",
+          color: "transparent", 
+        }}
+        className="font-space-grotesk font-bold"
+        repeat={Infinity}
+        cursor={true}
+      />
       <p className='font-space-grotesk text-text-low-contrast'>I design <span className='font-bold text-text'>colorful</span> interfaces and develop <span className='font-bold text-text'>dynamic</span> web and mobile applications.</p>
     </section>
   )
