ee7a66a410c74f06c823640a8c41bfef9dbffbae
Ella <ellalala.mc@gmail.com>
2025-07-26 22:14:14 +0800

    feat: :sparkles: Add new sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import HeroSection from "./components/HeroSection"
+import AboutSection from "./sections/AboutSection";
+import HeroSection from "./sections/HeroSection"
 import ScrollFadeIn from "./components/ScrollFadeIn";
+import ExperienceSection from "./sections/ExperienceSection";
 
 function App() {
   return (
     <>
       <ScrollFadeIn className="py-[100px] px-16 lg:py-60 lg:px-80">
         <HeroSection />
+      </ScrollFadeIn>
+
+      <ScrollFadeIn className="py-[100px] px-8 lg:py-60 lg:px-80">
+        <AboutSection />
+      </ScrollFadeIn>
+
+      <ScrollFadeIn className="py-[100px] px-8 lg:py-60 lg:px-80">
+        <ExperienceSection />
       </ScrollFadeIn>
     </>
   )
