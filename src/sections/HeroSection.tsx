9f66e51969c444fb5b00163db1cfb0dfeb11c0af
Ella <ellalala.mc@gmail.com>
2025-07-27 19:41:59 +0800

    style: Add more content

--- src/sections/HeroSection.tsx
+++ src/sections/HeroSection.tsx
@@ -3,7 +3,7 @@ import { TypeAnimation } from "react-type-animation";
 const HeroSection = () => {
   return (
     <section className='flex flex-col items-center justify-center gap-y-3 lg:gap-y-6'>
-      <p className='text-accent font-space-grotesk text-base lg:text-xl'>Hi, I'm Ella</p>
+      <p className='text-accent font-space-grotesk text-base lg:text-lg'>👋 Hi, I'm Ella</p>
       <TypeAnimation 
         sequence={[
           'A web and mobile developer',
@@ -25,14 +25,15 @@ const HeroSection = () => {
         repeat={Infinity}
         cursor={true}
       />
-      <p className='font-space-grotesk text-center text-text-low-contrast text-base lg:text-2xl lg:w-2/3'>
+      <p className='font-space-grotesk text-center text-text-low-contrast text-base lg:text-lg lg:w-2/3'>
         I design 
         <span className='font-bold text-text'> colorful </span> 
         interfaces and develop 
         <span className='font-bold text-text'> dynamic </span> 
-        web and mobile applications.
+        web and mobile applications. I'm actively involved in my university's tech startup community as a <span className="font-bold text-text">front-end developer</span>,
+        and developer student community as a <span className="font-bold text-text">former campus lead</span>.
       </p>
-      <button className="mt-6 bg-primary font-sora text-text rounded-2xl py-3 px-16 text-base lg:text-lg">
+      <button className="mt-6 bg-primary font-sora text-text rounded-2xl py-3 px-16 text-base">
         Contact me
       </button>
     </section>
