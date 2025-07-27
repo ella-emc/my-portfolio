37ef4ba519a405b87cb00119ff0e10ec21173fbf
Ella <ellalala.mc@gmail.com>
2025-07-27 17:40:39 +0800

    style: :lipstick: Add a hover zoom effect

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,7 @@
+// TO-DO:
+// 1. Make navbar appear when scrolled down
+// 2. Design navbar for smaller screens
+
 const Navbar = () => {
   const sections = ["about", "experience", "projects", "community involvements", "contact me"];
 
@@ -5,7 +9,7 @@ const Navbar = () => {
     <nav className='flex flex-row justify-center gap-6 glassmorph max-w-2/3 py-4 px-8 rounded-b-2xl'>
       {sections.map((section, index) => (
         <div key={index} className='flex flex-row items-center justify-center gap-6'>
-          <div className='text-text-low-contrast font-space-grotesk text-sm'>{section}</div>
+          <div className='text-text-low-contrast font-space-grotesk text-sm hover-zoom'>{section}</div>
           {index == sections.length - 1 ? "" : (
             <div className='text-text-ultra-low-contrast'>//</div>
           )}
