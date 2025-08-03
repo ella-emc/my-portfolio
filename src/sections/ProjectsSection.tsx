32490137d2e1dbb4dd98d798af1fea9eda238c44
Ella <ellalala.mc@gmail.com>
2025-08-03 19:21:48 +0800

    style: :lipstick: Modify layout

--- src/sections/ProjectsSection.tsx
+++ src/sections/ProjectsSection.tsx
@@ -6,10 +6,10 @@ const ProjectsSection = () => {
   return (
     <section>
       <h2 id="projects" className='font-space-grotesk text-primary text-center text-base lg:text-2xl mb-8 lg:mb-16'>Projects</h2>
-    
-      <div className="grid grid-cols-12 gap-6">
+
+      <div className="flex flex-row flex-wrap gap-y-16 gap-x-8">
         {projects.map((project, index) => (
-          <div key={index} className='col-span-6 flex flex-col gap-y-6'>
+          <div key={index} className='w-full md:w-[calc(50%-16px)] flex flex-col gap-y-6'>
             <div className="relative max-w-full max-h-64 overflow-hidden rounded-2xl group">
               <a href={project.link} target="_blank">
                 <img
@@ -18,7 +18,7 @@ const ProjectsSection = () => {
                   className="w-full h-full object-cover transform transition-transform duration-500 ease-in-out group-hover:scale-110"
                 />
               </a>
-              <div className="absolute inset-0 bg-purple-700 opacity-40 group-hover:opacity-0 transition-opacity duration-500 z-10 pointer-events-none"></div>
+              <div className="absolute inset-0 md:bg-purple-700 md:opacity-40 group-hover:opacity-0 transition-opacity duration-500 z-10 pointer-events-none"></div>
             </div>
             <div className='space-y-4'>
               <p className='text-text font-space-grotesk font-medium text-base lg:text-xl'>{project.title}</p>
