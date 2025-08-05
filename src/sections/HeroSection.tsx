cc8214061a22e55810a93c148b1d7ab8616b353f
Ella <ellalala.mc@gmail.com>
2025-08-05 23:03:00 +0800

    refactor: :recycle: Change header animation

--- src/sections/HeroSection.tsx
+++ src/sections/HeroSection.tsx
@@ -1,30 +1,35 @@
 import { TypeAnimation } from "react-type-animation";
+import RotatingText from "../components/RotatingText";
 
 const HeroSection = () => {
   return (
-    <section className='h-screen flex flex-col items-center justify-center px-12 lg:px-56 gap-y-3 lg:gap-y-6'>
+    <section className='h-screen flex flex-col items-center justify-center px-12 lg:px-56 gap-y-3 lg:gap-y-12'>
       <p className='text-accent font-space-grotesk text-base lg:text-lg'>👋 Hi, I'm Ella</p>
-      <TypeAnimation 
-        sequence={[
-          'A web and mobile developer',
-          1500,
-          'A full-stack developer',
-          1500,
-          'A data science enthusiast',
-          1500
-        ]}
-        wrapper="span"
-        speed={{ type: "keyStrokeDelayInMs", value: 90 }}
-        style={{
-          backgroundImage: "linear-gradient(to right, #A35CF0 0%, #F0FF6F 100%)",
-          WebkitBackgroundClip: "text", 
-          backgroundClip: "text",
-          color: "transparent", 
-        }}
-        className="font-space-grotesk font-bold text-center leading-tight text-[42px] lg:text-6xl"
-        repeat={Infinity}
-        cursor={true}
-      />
+      <div className="flex flex-row flex-wrap justify-center items-center gap-x-6">
+        <RotatingText
+          texts={['Mobile', 'Web', 'Full Stack', 'AI']}
+          mainClassName="px-2 sm:px-2 md:px-3 bg-primary text-[42px] lg:text-6xl text-background font-space-grotesk font-bold overflow-hidden py-0.5 sm:py-1 md:py-2 justify-center rounded-lg"
+          staggerFrom={"last"}
+          initial={{ y: "100%" }}
+          animate={{ y: 0 }}
+          exit={{ y: "-120%" }}
+          staggerDuration={0.025}
+          splitLevelClassName="overflow-hidden pb-0.5 sm:pb-1 md:pb-1"
+          transition={{ type: "spring", damping: 30, stiffness: 400 }}
+          rotationInterval={3000}
+        />
+        <p 
+          style={{
+            backgroundImage: "linear-gradient(to right, #A35CF0 0%, #F0FF6F 100%)",
+            WebkitBackgroundClip: "text", 
+            backgroundClip: "text",
+            color: "transparent", 
+          }}
+          className="text-[42px] lg:text-6xl font-space-grotesk font-bold text-text"
+        >
+          Developer
+        </p>
+      </div>
       <p className='font-space-grotesk text-center text-text-low-contrast text-base lg:text-lg lg:w-2/3'>
         I design 
         <span className='font-bold text-text'> colorful </span> 
