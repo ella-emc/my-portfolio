89ec126d33f41100fb8b1d07af660dedb97c651d
Ella <ellalala.mc@gmail.com>
2025-08-06 22:12:24 +0800

    feat: :sparkles: Create SpotlightCard component

---
+++ src/components/SpotlightCard.tsx
@@ -0,0 +1,79 @@
+import React, { useRef, useState } from "react";
+
+interface Position {
+  x: number;
+  y: number;
+}
+
+interface SpotlightCardProps extends React.PropsWithChildren {
+  className?: string;
+  spotlightColor1?: `rgba(${number}, ${number}, ${number}, ${number})`;
+  spotlightColor2?: `rgba(${number}, ${number}, ${number}, ${number})`;
+}
+
+const SpotlightCard: React.FC<SpotlightCardProps> = ({
+  children,
+  className = "",
+  spotlightColor1 = "rgba(255, 255, 255, 0.25)",
+  spotlightColor2 = "rgba(255, 255, 255, 0.25)"
+}) => {
+  const divRef = useRef<HTMLDivElement>(null);
+  const [isFocused, setIsFocused] = useState<boolean>(false);
+  const [position, setPosition] = useState<Position>({ x: 0, y: 0 });
+  const [position2, setPosition2] = useState<Position>({ x: 0, y: 0 }); 
+  const [opacity, setOpacity] = useState<number>(0);
+
+  const handleMouseMove: React.MouseEventHandler<HTMLDivElement> = (e) => {
+    if (!divRef.current || isFocused) return;
+
+    const rect = divRef.current.getBoundingClientRect();
+    const newPosition = { x: e.clientX - rect.left, y: e.clientY - rect.top };
+    setPosition(newPosition);
+    setPosition2({
+      x: newPosition.x - 50,
+      y: newPosition.y + 50
+    });
+  };
+
+  const handleFocus = () => {
+    setIsFocused(true);
+    setOpacity(0.6);
+  };
+
+  const handleBlur = () => {
+    setIsFocused(false);
+    setOpacity(0);
+  };
+
+  const handleMouseEnter = () => {
+    setOpacity(0.6);
+  };
+
+  const handleMouseLeave = () => {
+    setOpacity(0);
+  };
+
+  return (
+    <div
+      ref={divRef}
+      onMouseMove={handleMouseMove}
+      onFocus={handleFocus}
+      onBlur={handleBlur}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      className={`relative rounded-3xl overflow-hidden ${className}`}
+    >
+      <div
+        className="pointer-events-none absolute inset-0 opacity-0 transition-opacity duration-500 ease-in-out"
+        style={{
+          opacity,
+          background: `radial-gradient(circle at ${position.x}px ${position.y}px, ${spotlightColor1}, transparent 80%),
+                       radial-gradient(circle at ${position2.x}px ${position2.y}px, ${spotlightColor2}, transparent 80%)`
+        }}
+      />
+      {children}
+    </div>
+  );
+};
+
+export default SpotlightCard;
